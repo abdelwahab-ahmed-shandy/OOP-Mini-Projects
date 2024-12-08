554037912e95d4fa964aab27f9651bd872aa3ed7
Abdelwahab Ahmed Shandy <119771784+Abdelwahab-Shandy@users.noreply.github.com>
2024-12-08 18:23:11 +0200

    Add files via upload

---
+++ Bank management system/SavingsAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_management_system
+{
+    public class SavingsAccount : Account
+    {
+        public double InterstRate { get; set; }
+
+        public SavingsAccount(string name = "null", double balance = 0.0, double interstRate = 0.0) : base(name, balance)
+        {
+            if (interstRate < 0)
+                throw new Exception("Can Not Be Less Than Zero");
+            InterstRate = interstRate;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, Interest Rate: {InterstRate}";
+        }
+    }
+}
